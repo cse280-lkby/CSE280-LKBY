b6308135f72ca4a011713920fa0e2a03cc7be9ff
John Kusner <jkusner321@gmail.com>
2019-04-11 17:37:50 -0400

    Starting to work on socketio connection stuff

--- react-native/GroupChatApp/App.tsx
+++ react-native/GroupChatApp/App.tsx
@@ -10,6 +10,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import SocketIOClient from 'socket.io-client';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,13 +19,58 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const SIGNALING_SERVER = "http://ec2-54-164-202-125.compute-1.amazonaws.com:8080";
+const USE_AUDIO = true;
+const USE_VIDEO = false;
+const DEFAULT_CHANNEL = 'main_support_channel';
+const MUTE_AUDIO_BY_DEFAULT = false;
+
+const ICE_SERVERS = [
+    {urls:"stun:stun.l.google.com:19302"}
+];
+
+let signaling_socket = null;   /* our socket.io connection to our webserver */
+let local_media_stream = null; /* our own microphone / webcam */
+const peers = {};                /* keep track of our peer connections, indexed by peer_id (aka socket.io id) */
+const peer_media_elements = {};  /* keep track of our <video>/<audio> tags, indexed by peer_id */
+
+
+
 interface Props {}
-export default class App extends Component<Props> {
+interface State {
+  connected: boolean;
+}
+export default class App extends Component<Props, State> {
+  signaling_socket: SocketIOClient.Socket;
+  state = {
+    connected: false,
+  };
+  constructor(props: Props) {
+    super(props);
+
+    console.warn('Connecting?? :(');
+    this.signaling_socket = SocketIOClient(SIGNALING_SERVER);
+    this.signaling_socket.on('connect', () => {
+      console.warn("Connected!");
+      this.setState(state => ({
+        ...state,
+        connected: true
+      }));
+    });
+    
+    this.signaling_socket.on('disconnect', () => {
+      console.warn("Disconnected!");
+      this.setState(state => ({
+        ...state,
+        connected: false
+      }));
+    });
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Chat app!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
+        <Text style={styles.instructions}>Socket connected?: {this.state.connected ? 'yes' : 'no'}</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
