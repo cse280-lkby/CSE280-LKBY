cb8a56646fc4937af6f21475028b5505bb9070f9
John Kusner <jkusner321@gmail.com>
2019-11-01 14:54:46 -0400

    Footer textbox and button

--- text-interface/src/Footer.js
+++ text-interface/src/Footer.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 function Footer() {
   return (
-        <div className="Footer">
-            Send message
-        </div>
-    );
-  }
-  
-  export default Footer;
+    <div className="Footer">
+      {/* <div className="Top">
+        Suggestions will go here
+      </div> */}
+      <div className="Bottom">
+        <input className="TextBox" type="text" placeholder="Write a message..."></input>
+        <button className="SendButton">Send</button>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
