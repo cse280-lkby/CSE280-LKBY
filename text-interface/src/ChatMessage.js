5634251b83e339ed30f192049f2540cef5979d1d
John Kusner <jkusner321@gmail.com>
2019-11-02 13:08:25 -0400

    Replace SSML breaks with line breaks

--- text-interface/src/ChatMessage.js
+++ text-interface/src/ChatMessage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+// Parse out SSML style <break/>s into separate lines of text
+function withBreaks(text) {
+  return text.split(/<break [^>]+>/);
+}
+
 function ChatMessage(props) {
   return (
     <div className={`ChatMessage ${props.msg.outgoing ? 'Outgoing' : 'Incoming'}`}>
-      {props.msg.text}
+      {withBreaks(props.msg.text).map(line => <div>{line}</div>)}
     </div>
   );
 }
