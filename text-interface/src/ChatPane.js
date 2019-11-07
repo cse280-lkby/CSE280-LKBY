cfe54000061dbc0c9e9d4c95c0183640b1ec78ce
John Kusner <jkusner321@gmail.com>
2019-11-07 13:10:32 -0500

    TI autoscroll

--- text-interface/src/ChatPane.js
+++ text-interface/src/ChatPane.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ChatMessage from './ChatMessage';
 
 function ChatPane(props) {
+  const paneRef = React.useRef();
+
+  // Autoscroll when messages arrive
+  React.useEffect(() => {
+    if (!paneRef.current) return;
+    paneRef.current.scrollTop = paneRef.current.scrollHeight;
+  }, [props.messages.length]);
+
   return (
-    <div className="ChatPane">
+    <div className="ChatPane" ref={paneRef}>
       {
         props.messages.map((msg, i) => <ChatMessage key={i} msg={msg} />)
       }
