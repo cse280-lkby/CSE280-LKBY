4f3de7b57c0f23c78fc210b8e6687a4469eae46e
John Kusner <jkusner321@gmail.com>
2019-11-12 16:38:35 -0500

    TI chat autoscroll on resize

--- text-interface/src/ChatPane.js
+++ text-interface/src/ChatPane.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ChatMessage from './ChatMessage';
 
+function scrollToBottom(paneRef) {
+  if (!paneRef.current) return;
+  paneRef.current.scrollTop = paneRef.current.scrollHeight;
+}
+
 function ChatPane(props) {
   const paneRef = React.useRef();
 
   // Autoscroll when messages arrive
   React.useEffect(() => {
-    if (!paneRef.current) return;
-    paneRef.current.scrollTop = paneRef.current.scrollHeight;
+    scrollToBottom(paneRef);
   }, [props.messages.length]);
+
+  // Autoscroll when window resizes
+  React.useEffect(() => {
+    const eventListener = () => {
+      scrollToBottom(paneRef);
+    };
+
+    window.addEventListener('resize', eventListener);
+
+    return () => {
+      window.removeEventListener('resize', eventListener);
+    }
+  }, []);
 
   return (
     <div className="ChatPane" ref={paneRef}>
